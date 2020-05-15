393185a283b79c6b11b5b4c67861ba04ff9d6a5f
Oksana <myoksik@gmail.com>
2020-05-16 01:17:31 +0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+let incr = 10,
+decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
